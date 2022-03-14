cd59e80bf2fb631f60ccb5b7ba83fa06113714fa
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-03-14 20:57:14 +1030

    startup question added

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // npm packages induce
 const path = require("path")
 const fs = require("fs")
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const inquirer = require("inquirer");
 
-// routing build up
+// routing to mysql database build up
 const db = mysql.createConnection(
     {
       host: 'localhost',
@@ -14,4 +14,32 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the tracker_db database.`)
 );
-  
+
+db.connect(function (err) {
+    if (err) throw err;
+    startquestion();
+});
+
+function startquestion() {
+    inquirer.prompt([
+        {
+            name: "addViewUpdate",
+            type: "rawlist",
+            message: "To build up employee database, which action would you like to take?",
+            choices: ["Add", "View", "Update"]
+        }])
+        .then((res) => {
+            switch (res.addViewUpdate) {
+                case "Add":
+                    addRole();
+                    break;
+                case "View":
+                    viewRole();
+                    break;
+                case "Update Employee Role":
+                    updateRole();
+                    break;
+                default: console.log("Please choose from the list.")
+            }
+        })
+}
