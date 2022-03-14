72025296b8bf19262fd387c418122a8ea062fe4b
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-03-14 22:41:38 +1030

    Tracker structure correct

--- db/tracker.sql
+++ db/tracker.sql
@@ -20,7 +20,10 @@ CREATE TABLE employee (
     id INT (10),
     first_name VARCHAR (30),
     last_name VARCHAR (30),
-    role_id INT (10),
+    role_id INT (10)
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE CASCADE,
     manager_name VARCHAR (30),
     manager_id INT (10)
     FOREIGN KEY (manager_id)
