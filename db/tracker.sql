197f59b7d18502fcd6c7aa8c62150a705ce9aa34
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-03-14 21:15:20 +1030

    Resturctured tracker table connect

--- db/tracker.sql
+++ db/tracker.sql
@@ -3,13 +3,18 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 CREATE TABLE department (
     id INT (10),
-    name VARCHAR (50)
+    name VARCHAR (50),
+    PRIMARY KEY (`id`)
 );
 CREATE TABLE role (
     id INT (10),
     title VARCHAR (30),
     salary FLOAT (10),
-    department_id INT (10)
+    department_id INT (10),
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE CASCADE
+    PRIMARY KEY (`id`) 
 );
 CREATE TABLE employee (
     id INT (10),
@@ -17,4 +22,8 @@ CREATE TABLE employee (
     last_name VARCHAR (30),
     role_id INT (10),
     manager_id INT (10)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+    PRIMARY KEY (`id`)
 );
