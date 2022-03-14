0e57aa822ff16ce74fbb4f7a5c6447f0c00b427d
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-03-14 20:19:40 +1030

    routing build up

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ const path = require("path")
 const fs = require("fs")
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+// routing build up
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'tracker_db'
+    },
+    console.log(`Connected to the tracker_db database.`)
+);
+  
