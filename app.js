29b1b0d339d4c816713a178f8a256d270f259dbe
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-05 12:47:17 +0930

    type err

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 // npm packages induce
-const path = require("path")
-const fs = require("fs")
+// Take charge of directory path, See documentation at https://nodejs.org/docs/latest/api/path.html
+// const path = require("path")
+
+// file-system alais fs Provide basic functions in node.js, See documentation at https://www.npmjs.com/package/file-system
+// const fs = require("fs")
+
+// easy and fast way of applying mysql, examples provided here at https://www.npmjs.com/package/mysql2
 const mysql = require("mysql2");
+
+// Tool to handle questions and responses in node.js, readme instruction only at https://github.com/SBoudrias/Inquirer.js#readme
 const inquirer = require("inquirer");
+
+// Adds console.table method for convenience of selecting (reading), at https://www.npmjs.com/package/console.table
 const cTable = require('console.table');
 
-// routing to mysql database build up
+// routing to mysql database build up, supported by mysql2
 const db = mysql.createConnection(
     {
       host: 'localhost',
@@ -51,13 +60,13 @@ function startquestion() {
 function add() {
     inquirer.prompt([
         {
-            name: "employeeRoleDepartment",
+            name: "primaryQuestion",
             type: "list",
             message: "What would you like to add?",
             choices: ["Employee", "Role", "Department"]
         }])
         .then((res) => {
-            switch (res.employeeRoleDepartment) {
+            switch (res.primaryQuestion) {
                 case "Employee":
                     addEmployee();
                     break;
@@ -75,6 +84,11 @@ function add() {
     function addEmployee() {
         inquirer.prompt([
             {
+                name: "id",
+                type: "number",
+                message: "What is the employee's id?"
+            },
+            {
                 name: "first_name",
                 type: "input",
                 message: "What is the employee's first name?"
@@ -91,42 +105,29 @@ function add() {
             },
             {
                 name: "manager_id",
-                type: "input",
-                message: "What is the manager's id number?"
-            },
-            {
-                name: "id",
                 type: "number",
-                message: "What is the employee's id?"
-            },
+                message: "What is the manager's id number?"
+            }
         ])
-        //update mysql table and display in console.table
-        .then(function (res) {
-            console.table(res)
-            let query = connection.query("INSERT INTO employee SET ?",
+       .then(function (res) {
+            db.query('INSERT INTO employee SET ?',
                 {
+                    id: res.id,
                     first_name: res.first_name,
                     last_name: res.last_name,
                     role_id: res.role_id,
                     manager_id: res.manager_id,
-                    id: res.id
-
                 },
-                function (err, res) {
-                    if (err) throw err
-                    console.log(res)
-                })
-        startquestion();
-        })
+                (err, res) => {
+                    if (err) throw err;
+                });
+            startquestion();
+            }
+        )
     }
 
     function addRole() {
         inquirer.prompt([
-            {
-                name: "ID_PK",
-                type: "number",
-                message: "What is the ID of the role?"
-            },
             {
                 name: "title",
                 type: "input",
@@ -143,18 +144,16 @@ function add() {
                 message: "What is the id of department that this role belongs to?"
             },
         ])
-        .then((res) => {
-            let query = db.query("INSERT INTO role SET ?",
+        .then(function (res) {
+            db.query('INSERT INTO role SET ?',
             {
-                ID_PK: res.ID_PK,
                 title: res.title,
                 salary: res.salary,
                 department_id: res.department_id
             },
             (err, res) => {
-                if (err) throw err
-                console.log(res)
-            })
+                if (err) throw err;
+            });
             startquestion();
         })
     }
@@ -162,26 +161,19 @@ function add() {
     function addDepartment() {
         inquirer.prompt([
             {
-                name: "ID_PK",
-                type: "number",
-                message: "What is the department ID?"
-            },
-            {
                 name: "name",
                 type: "input",
                 message: "What is name of the department?"
             }
         ])
-        .then((res) => {
-            let query = db.query("INSERT INTO role SET ?",
+        .then(function (res) {
+            db.query('INSERT INTO department SET ?',
             {
-                ID_PK: res.ID_PK,
-                    name: res.name,
+                name: res.name
             },
             (err, res) => {
                 if (err) throw err
-                console.log(res)
-            })
+            });
             startquestion();
         })
     }
@@ -211,7 +203,12 @@ function view() {
     )
     
     function viewEmployee() {
-        let viewEmployeeQuery = "SELECT id, first_name, last_name, title AS role, name AS department, salary, manager_name AS manager FROM employee E LEFT JOIN role R on E.role_id = R.ID_PK JOIN department D on D.ID_PK = R.ID_PK";
+        // Name concat at https://stackoverflow.com/questions/20284528/how-to-concat-two-columns-into-one-with-the-existing-column-name-in-mysql
+        let viewEmployeeQuery = `SELECT E.id, E.first_name, E.last_name, R.title AS role, D.name AS department, R.salary, CONCAT(ER.first_name, ' ', ER.last_name) AS manager_name
+        FROM employee E 
+        LEFT JOIN role R on E.role_id = R.id
+        LEFT JOIN department D on D.id = R.department_id
+        LEFT JOIN employee ER on E.manager_id = ER.id`;
         db.query(viewEmployeeQuery, function (err, res) {
             console.table(res);
             startquestion();
@@ -219,7 +216,7 @@ function view() {
     }
     
     function viewDepartment() {
-        let viewDepartmentQuery = "SELECT ID_PK, name AS department_name FROM department";
+        let viewDepartmentQuery = "SELECT id, name AS department_name FROM department";
         db.query(viewDepartmentQuery, function (err, res) {
             console.table(res);
             startquestion();
@@ -227,7 +224,8 @@ function view() {
     }
     
     function viewRole() {
-        let viewRoleQuery = "SELECT ID_PK, title AS role_title, name AS department, salary FROM role R LEFT JOIN department D on D.ID_PK = R.department_id";
+        //Same id refernce solution https://stackoverflow.com/questions/35968229/selecting-records-with-the-same-name-give-duplicate-results
+        let viewRoleQuery = `SELECT R.id, R.title AS role_title, D.name AS department, R.salary FROM role R LEFT JOIN department D on R.department_id = D.id`
         db.query(viewRoleQuery, function (err, res) {
             console.table(res);
             startquestion();
@@ -261,8 +259,8 @@ function updateEmployeeRole() {
                 }
             ]).then(({ targetEmp, targetRole }) => {
                 let id = targetEmp.split(":")[0]; // targetEmp = "6: Dwifht Shrute"
-                let role_ID = targetRole.split(":")[0];
-                db.query("UPDATE employee SET role_ID = ? WHERE id = ?", [role_ID, id], (err, success) => {
+                let role_id = targetRole.split(":")[0];
+                db.query("UPDATE employee SET role_id = ? WHERE id = ?", [role_id, id], (err, success) => {
                     if (err) throw err;
                     console.log(`Your changes were successful`);
                     startquestion();
