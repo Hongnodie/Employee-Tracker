97d5b5e2e114f2995e30f2b214064646a03b2d49
Hongnodie <95200204+Hongnodie@users.noreply.github.com>
2022-04-05 12:42:26 +0930

    tracker fix

--- db/tracker.sql
+++ db/tracker.sql
@@ -2,19 +2,21 @@ DROP DATABASE IF EXISTS tracker_db;
 CREATE DATABASE tracker_db;
 USE tracker_db;
 CREATE TABLE department (
-    ID_PK INT (10),
+    id INT PRIMARY KEY,
     name VARCHAR (50)
 );
 CREATE TABLE role (
-    ID_PK INT (10),
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR (30),
     salary DECIMAL (10),
     department_id INT (10)
 );
 CREATE TABLE employee (
-    id INT (10),
+    id INT PRIMARY KEY,
     first_name VARCHAR (30),
     last_name VARCHAR (30),
     role_id INT (10),
     manager_id INT (10)
 );
+-- The IDENTITY keyword creates an autonumber column that automatically increments the Id when a new record is added, should not be commented in the codes
+-- PRIMARY KEY
